7589c0202ca375dc9f36962028a489be4b5e1a68
Huy Tran <huytran172@gmail.com>
2023-08-17 11:05:19 -0400

    fix: Typo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if(env('APP_ENV') !== 'local')
         {
-            $url->forceSchema('https');
+            $url->forceScheme('https');
         }
     }
 }
